f99215a589a38a281bd2959e63b0213e653b9e1b
Intersession <intersession@example.com>
2026-05-04 15:22:13 -0700

    Move user changes into design-jeff

---
+++ maps/smeegle.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="smeegle" tilewidth="64" tileheight="64" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../assets/2d/Background/Blue.png" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="../assets/2d/Background/Brown.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../assets/2d/Background/Gray.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../assets/2d/Background/Green.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="../assets/2d/Background/Grid.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../assets/2d/Background/Pink.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="../assets/2d/Background/Purple.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../assets/2d/Background/Yellow.png" width="64" height="64"/>
+ </tile>
+</tileset>
